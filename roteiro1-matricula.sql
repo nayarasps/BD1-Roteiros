f51061d94f2111078dd43ebdab5aa043d306dc65
Nayara Souza <44266971+nayarasps@users.noreply.github.com>
2020-03-16 09:45:38 -0300

    Add files via upload

--- roteiro1-matricula.sql
+++ roteiro1-matricula.sql
@@ -1,36 +1,36 @@
 -- @author Nayara Souza - 118110390
 
 -- Questão 1
--- Considerando 
+-- Criada uma situação em que esse banco de dados pertence a uma seguradora
+
+-- 
+CREATE TABLE Seguro (
+Contrato SERIAL,
+Segurado_Cpf CHAR(7),
+Automovel_Placa CHAR(7),
+Preço NUMERIC,
+Serviços TEXT
+);
 
 CREATE TABLE Automovel (
-Placa CHAR(7)
-Chassi CHAR(17)
-Modelo VARCHAR(50)
-Marca VARCHAR(50)
-Ano INTEGER
+Placa CHAR(7),
+Chassi CHAR(17),
+Modelo VARCHAR(50),
+Marca VARCHAR(50),
+Ano INTEGER,
 Segurado_cpf INTEGER
-)
+);
 
 CREATE TABLE Segurado (
-Cpf INTEGER
-Cnh INTEGER
-
-)
+Cpf INTEGER,
+Cnh INTEGER,
+);
 
 CREATE TABLE Perito (
-Cpf
-Automovel_Placa
-)
-
-CREATE TABLE Seguro (
-Contrato
-Segurado_Cpf
-Automovel_Placa
-Preço
-Serviços 
-)
+Cpf INTEGER,
+Automovel_Placa CHAR(7)
+);
 
 CREATE TABLE Oficina (
-
+);
 
